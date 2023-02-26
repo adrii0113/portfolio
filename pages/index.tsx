425ecab63909e3b22893c6204ee5664e9534a334
adrii0113 <adrian190401@gmail.com>
2023-02-26 12:28:38 +0100

    testing some new hover features

--- pages/index.tsx
+++ pages/index.tsx
@@ -76,7 +76,7 @@ export default function Home() {
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
               Junior fullstack developer.
             </h3>
-            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl hover:bg-slate-600">
+            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
             
               Developer with less than 1 year of experience, actually learning and improving my development skills, scroll down to discover more about my projects
             </p>
