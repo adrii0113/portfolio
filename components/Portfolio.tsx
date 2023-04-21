bf8acebec540326f4c9609619241605ddb826418
adrii0113 <adrian190401@gmail.com>
2023-04-21 21:38:10 +0200

    add some info about projects, add footer info

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -32,17 +32,26 @@ export default function Portofolio(){
           </p> */}
         </div>
         <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-          <motion.div className="basis-1/3 flex-1 " whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+          <motion.div className="basis-1/3 flex-1 group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
             <a href="https://socialapp-ruddy.vercel.app/">
             <Image
-              className="rounded-lg object-cover"
+              className="rounded-lg h-full w-full object-cover transition-transform duration-500"
               alt=''
               layout="responsive"
               src={socialapp}
+              
             />
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[50%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+              <h1 className="font-dmserif text-3xl font-bold text-white">Social App</h1>
+             <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Pagina web realizada como proyecto final, red social completa, utilizando el stack MERN.</p>
+              <button className="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">Visitar web</button>
+            </div>
+            
             </a>
+            
           </motion.div>
-          <motion.div className="basis-1/3 flex-1" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+          <motion.div className="basis-1/3 flex-1 group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
             <a href="https://moviewebappclone.netlify.app/">
             <Image
               className="rounded-lg object-cover hover:hover08"
@@ -50,6 +59,12 @@ export default function Portofolio(){
               layout="responsive"
               src={moviefinder}
             />
+             <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[50%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+              <h1 className="font-dmserif text-3xl font-bold text-white">Movie clon</h1>
+             <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Pagina web realizada como proyecto final, red social completa, utilizando el stack MERN.</p>
+              <button className="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">Visitar web</button>
+            </div>
             </a>
           </motion.div>
           {/* <motion.div className="basis-1/3 flex-1" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
